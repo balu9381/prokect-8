181378a5e752067fd074fbdb8d2c2c6acdc5bfb8
balu9381 <baluravi215@gmail.com>
2022-12-30 21:43:24 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def balu(request):
+    return render(request,'balu.html')
+
+def bala(request):
+    return render(request,'bala.html')
